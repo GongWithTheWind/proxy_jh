7be8abbcabc810a39c271ff0f8dc8cac253067d1
Jenn <huynhjennt@gmail.com>
2018-09-20 15:24:50 -0700

    reroute booking through proxy

--- proxy/server.js
+++ proxy/server.js
@@ -4,8 +4,26 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// const proxy = require('http-proxy-middleware');
+// const { routes } = require('./config.json');
+
 app.use(morgan('dev'));
 app.use('/:homeId', express.static(path.join(__dirname, 'public')));
+
+// for (route of routes) {
+//   app.use(route.route,
+//       proxy({
+//           target: route.address
+//       })
+//   );
+// }
+
+app.use('/booking/:id', proxy({ 
+  target: 'http://localhost:3004/house/100',
+  pathRewrite: (path, req) => {
+    return path.split('/').slice(2);
+  }
+}));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
