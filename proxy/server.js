f4ab1898fd538096769fe22c83288dbabd447418
Jenn <huynhjennt@gmail.com>
2018-09-20 16:41:43 -0700

    routing inprogress

--- proxy/server.js
+++ proxy/server.js
@@ -4,27 +4,47 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// const proxy = require('http-proxy-middleware');
-// const { routes } = require('./config.json');
+const proxy = require('http-proxy-middleware');
+const { routes } = require('./config.json');
 
 app.use(morgan('dev'));
-app.use('/:homeId', express.static(path.join(__dirname, 'public')));
+app.use('/home/:homeId', express.static(path.join(__dirname, 'public')));
 
-// for (route of routes) {
-//   app.use(route.route,
-//       proxy({
-//           target: route.address
-//       })
-//   );
-// }
+// app.use('/booking/:id', proxy({ 
+//   target: 'http://localhost:3004/',
+//   pathRewrite: (path, req) => {
+//     return path.replace('/booking', '/house');
+//   }
+// }));
 
-app.use('/booking/:id', proxy({ 
-  target: 'http://localhost:3004/house/100',
-  pathRewrite: (path, req) => {
-    return path.split('/').slice(2);
-  }
-}));
+for (route of routes) {
+  app.use(route.route,
+      proxy({
+          target: route.address,
+          pathRewrite: (path, req) => {
+            return path.replace(route.source, route.reroute);
+          }
+      })
+  );
+}
+
+// app.use('/gallery/:id', proxy({ 
+//   target: 'http://localhost:3003/',
+//   pathRewrite: (path, req) => {
+    
+//     let newPath = path.split('/').slice(2, 3);
+//     console.log(newPath);
+//     // newPath = newPath.replace('gallery', 'homes');
+//     return newPath;
+//   }
+// }));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });
+
+// component changes for routes:
+
+// change /house route to /booking in booking.jsx
+// change /homes route to /gallery in App.js for gallery
+  // take out /images from route
