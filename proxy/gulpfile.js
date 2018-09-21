7716830e8264c1861af99272f3038d8f83c1b6a7
Jenn <huynhjennt@gmail.com>
2018-09-21 14:34:48 -0700

    host bundles on s3

---
+++ proxy/gulpfile.js
@@ -0,0 +1,7 @@
+// var run = require('gulp-run');
+
+// // use gulp-run to start a pipeline 
+// gulp.task('exec_all_scripts', function() {
+//     return run('npm start').exec();    // run "npm start". 
+//         // .pipe(gulp.dest('output'));      // writes results to output/echo. 
+// })
